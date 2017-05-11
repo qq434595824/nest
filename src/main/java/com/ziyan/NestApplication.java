f1f36e508378f667e19f9bc51fa82d5a1a858594
子炎 <zhengmengyan@taoqicar.com>
2017-05-11 16:04:39 +0800

    fix

--- src/main/java/com/ziyan/NestApplication.java
+++ src/main/java/com/ziyan/NestApplication.java
@@ -1,18 +1,37 @@
 package com.ziyan;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 @ImportResource("classpath*:/spring-shiro.xml")
 @EnableAutoConfiguration
 @SpringBootApplication
 public class NestApplication {
+	private static final Logger log = LoggerFactory.getLogger(NestApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(NestApplication.class, args);
+	public static void main(String[] args) throws UnknownHostException {
+		Environment env = SpringApplication.run(NestApplication.class, args).getEnvironment();
+		log.info(
+				"\n----------------------------------------------------------\n\t"
+						+ "Application '{}' is running! Access URLs:\n\t" + "Local: \t\thttp://127.0.0.1:{}\n\t"
+						+ "External: \thttp://{}:{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"), env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(), env.getProperty("server.port"));
+
+		String configServerStatus = env.getProperty("configserver.status");
+		log.info(
+				"\n----------------------------------------------------------\n\t"
+						+ "Config Server: \t{}\n----------------------------------------------------------",
+				configServerStatus == null ? "Not found or not setup for this application" : configServerStatus);
 	}
 }
